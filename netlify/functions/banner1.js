91bbd338b7b8ddbd609134c1234bfd7f2a4386e2
Antonino Ragusa <antonino.ragusa.er@gmail.com>
2025-02-20 16:32:14 +0100

    Update banner1.js

--- netlify/functions/banner1.js
+++ netlify/functions/banner1.js
@@ -1,1 +1,20 @@
 
+exports.handler = async (event) => {
+    const referer = event.headers.referer || "";
+    
+    let imageUrl = "/lego2.png"; // Default image
+
+    if (referer.includes("legoforum.forumcommunity.net")) {
+        imageUrl = "/lego.png";
+    } else if (referer.includes("thesimsita.forumcommunity.net")) {
+        imageUrl = "/tllchallange.png";
+    }
+
+    return {
+        statusCode: 302,
+        headers: {
+            Location: imageUrl,
+        },
+        body: "",
+    };
+};
